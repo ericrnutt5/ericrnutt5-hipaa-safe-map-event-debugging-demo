247c39f2c64528c3c508f4e706a79c2cccb22884
ericnutt <ericrnutt1995@gmail.com>
2026-05-20 07:08:24 -0500

    updating go for other PII entities

--- backend/sanitize.go
+++ backend/sanitize.go
@@ -4,8 +4,22 @@ import "fmt"
 
 func sanitizeEvent(event map[string]string) map[string]string {
 
-	// Remove sensitive healthcare identifiers
-	delete(event, "patient_email")
+	// Remove sensitive healthcare identifiers / potential PHI
+	sensitiveFields := []string{
+		"patient_email",
+		"patient_phone",
+		"patient_name",
+		"ip_address",
+		"latitude",
+		"longitude",
+		"date_of_birth",
+		"medical_record_number",
+		"insurance_id",
+	}
+
+	for _, field := range sensitiveFields {
+		delete(event, field)
+	}
 
 	return event
 }
@@ -13,10 +27,17 @@ func sanitizeEvent(event map[string]string) map[string]string {
 func main() {
 
 	event := map[string]string{
-		"event":         "Viewed Oncology Clinic",
-		"clinic_name":   "Austin Cancer Center",
-		"patient_email": "patient@email.com",
-		"zip_code":      "78704",
+		"event":                 "Viewed Oncology Clinic",
+		"clinic_name":           "Austin Cancer Center",
+		"patient_email":         "patient@email.com",
+		"patient_phone":         "512-555-1234",
+		"patient_name":          "Jane Doe",
+		"ip_address":            "192.168.1.15",
+		"latitude":              "30.2672",
+		"longitude":             "-97.7431",
+		"medical_record_number": "MRN-48291",
+		"insurance_id":          "INS-77821",
+		"zip_code":              "78704",
 	}
 
 	fmt.Println("Original Event:")
